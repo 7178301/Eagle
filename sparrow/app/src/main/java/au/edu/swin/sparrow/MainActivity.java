0dab535702d8210dee0238f4614cd11577b56f8e
Cameron Cross <cameroncros@gmail.com>
2015-08-12 01:08:25 +1000

    Added first test, code should compile and run when a delay() function is added

--- sparrow/app/src/main/java/au/edu/swin/sparrow/MainActivity.java
+++ sparrow/app/src/main/java/au/edu/swin/sparrow/MainActivity.java
@@ -1,5 +1,6 @@
 package au.edu.swin.sparrow;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,24 +8,20 @@ import android.view.MenuItem;
 
 
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.SimpleAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 
-import dji.sdk.api.DJIDrone;
-import dji.sdk.api.DJIDroneTypeDef;
-import dji.sdk.api.DJIError;
-import dji.sdk.interfaces.DJIGerneralListener;
 import eagle.Drone;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +36,7 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -62,7 +60,7 @@ public class MainActivity extends ActionBarActivity {
         Log.e(TAG, "EagleAPI Version: " + drone.getAPIVersion());
         TextView tv = (TextView)findViewById(R.id.textviewVersion);
         tv.setText(drone.getAPIVersion());
-
+        //fill dropdown with adaptors
         HashSet<String> sdkAdaptors = drone.getSDKAdaptorList();
         Log.e(TAG, "SDK Adaptors: " + sdkAdaptors.toString());
 
@@ -70,6 +68,42 @@ public class MainActivity extends ActionBarActivity {
         List<String> arrayOfAdaptors = new ArrayList<String>();
         arrayOfAdaptors.addAll(sdkAdaptors);
 
-        sp.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayOfAdaptors));
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayOfAdaptors);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(ad);
+
+        //fill test dropdown with tests
+        List<String> arrayOfTests = new ArrayList<String>();
+        arrayOfTests.add("FlyUpFlyDown");
+        arrayOfTests.add("All Tests");
+
+        Spinner sp2 = (Spinner)findViewById(R.id.spinnerTests);
+        ArrayAdapter<String> ad2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayOfTests);
+        ad2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp2.setAdapter(ad2);
+
+
+
+
+        Button bt = (Button)findViewById(R.id.buttonRunTest);
+        bt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Spinner sp = (Spinner)findViewById(R.id.spinnerSDKs);
+        String sdk = sp.getSelectedItem().toString();
+
+        Spinner sp2 = (Spinner)findViewById(R.id.spinnerTests);
+        String test = sp2.getSelectedItem().toString();
+
+        Intent myIntent = new Intent(this, RunningTest.class);
+        myIntent.putExtra("sdk", sdk); //Optional parameters
+        myIntent.putExtra("test", test); //Optional parameters
+
+        startActivity(myIntent);
+
+
+
     }
 }
