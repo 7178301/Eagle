4005010f3c28ceef4c382dd5bcb5f45a27100a58
Cameron Cross <cameroncros@gmail.com>
2015-09-01 15:48:43 +1000

    Added tests for relative commands, removed unnecessary includes

--- eagle/eagleAPI/src/main/java/eagle/Drone.java
+++ eagle/eagleAPI/src/main/java/eagle/Drone.java
@@ -21,9 +21,7 @@ import eagle.sdkInterface.SDKAdaptor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
--- eagle/eagleAPI/src/test/java/eagle/DroneTest.java
+++ eagle/eagleAPI/src/test/java/eagle/DroneTest.java
@@ -1,6 +1,13 @@
 package eagle;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import eagle.navigation.positioning.Bearing;
+import eagle.navigation.positioning.Position;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * Drone Class Tester
@@ -13,9 +20,89 @@ import org.junit.Test;
  */
 
 public class DroneTest {
+    Drone drone;
+
+    @Before
+    public void beforeTest() {
+        drone = new Drone();
+        drone.setSDKAdaptor("Simulator.Simulator");
+    }
+
     @Test
-    public void testExecuteInstruction() throws Drone.InvalidInstructionException {
-//        SDKAdaptor sim = new Simulator();
-//        sim.executeInstruction("DELAY 1000");
+    public void testExecuteInstructionInvalid() {
+        try {
+            drone.executeInstruction("ASDF");
+            fail("Exception should be thrown");
+        } catch (Drone.InvalidInstructionException e) {
+        }
+    }
+
+    @Test
+    public void testExecuteInstructionConnectToDrone() {
+        try {
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("CONNECTTODRONE"));
+        }
+        catch (Drone.InvalidInstructionException e) {
+            fail("Invalid instruction");
+        }
+    }
+    @Test
+    public void testExecuteInstructionDisconnectToDrone() {
+        try {
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("DISCONNECTFROMDRONE"));
+        }
+        catch (Drone.InvalidInstructionException e) {
+            fail("Invalid instruction");
+        }
+    }
+
+    @Test
+    public void testExecuteInstructionFLYTORELATIVE() {
+        try {
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("FLYTORELATIVE 1 2 3 4"));
+            assertEquals("moved to wrong position", drone.getSDKAdaptor().getPositionAssigned().toString(), new Position(1, 2, 3, 0, 0, new Bearing(4)).toString());
+        }
+        catch (Drone.InvalidInstructionException e) {
+            fail("Invalid instruction");
+        }
+    }
+
+    @Test
+    public void testExecuteInstructionCHANGELONGITUDERELATIVE() {
+        try {
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("CHANGELONGITUDERELATIVE 1"));
+            assertEquals("moved to wrong position", drone.getSDKAdaptor().getPositionAssigned().toString(), new Position(1, 0, 0, 0, 0, new Bearing(0)).toString());
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("CHANGELONGITUDERELATIVE 2 1"));
+            assertEquals("moved to wrong position", drone.getSDKAdaptor().getPositionAssigned().toString(), new Position(3, 0, 0, 0, 0, new Bearing(0)).toString());
+        }
+        catch (Drone.InvalidInstructionException e) {
+            fail("Invalid instruction");
+        }
+    }
+
+    @Test
+    public void testExecuteInstructionCHANGELATITUDERELATIVE() {
+        try {
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("CHANGELATITUDERELATIVE 1"));
+            assertEquals("moved to wrong position", drone.getSDKAdaptor().getPositionAssigned().toString(), new Position(0, 1, 0, 0, 0, new Bearing(0)).toString());
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("CHANGELATITUDERELATIVE 2 1"));
+            assertEquals("moved to wrong position", drone.getSDKAdaptor().getPositionAssigned().toString(), new Position(0, 3, 0, 0, 0, new Bearing(0)).toString());
+        }
+        catch (Drone.InvalidInstructionException e) {
+            fail("Invalid instruction");
+        }
+    }
+
+    @Test
+    public void testExecuteInstructionCHANGEALTITUDERELATIVE() {
+        try {
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("CHANGEALTITUDERELATIVE 1"));
+            assertEquals("moved to wrong position", drone.getSDKAdaptor().getPositionAssigned().toString(), new Position(0, 0, 1, 0, 0, new Bearing(0)).toString());
+            assertEquals("return wrong value", "SUCCESS", drone.executeInstruction("CHANGEALTITUDERELATIVE 2 1"));
+            assertEquals("moved to wrong position", drone.getSDKAdaptor().getPositionAssigned().toString(), new Position(0, 0, 3, 0, 0, new Bearing(0)).toString());
+        }
+        catch (Drone.InvalidInstructionException e) {
+            fail("Invalid instruction");
+        }
     }
 }
